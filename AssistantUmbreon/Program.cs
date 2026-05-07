583ce4fa9576fd1a5fc0b82e1c7a77f9ad6a1de7
Tana <finaltruth.shintai@gmail.com>
2026-05-07 14:44:40 +0900

    ブートストラップロガーの生成タイミングを最初に変更。実装を理解するための解説コメントを追加。

--- AssistantUmbreon/Program.cs
+++ AssistantUmbreon/Program.cs
@@ -9,27 +9,28 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
+// ホスト構築前のエラーをコンソールに出力するために、最小構成のロガーを作成して使用する
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
 #if DEBUG
-// 開発ビルド時のみ .env ファイルがあれば読み込んで Environment クラスから環境変数として使用できるようにする
+// 開発ビルド時のみ .env ファイルがあれば読み込んで環境変数として使用できるようにする
 if (File.Exists(".env"))
 {
     Env.Load();
 }
 #endif
 
-// ホスト構築前のエラーをコンソールに出力するための最小限のブートストラップロガー
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .CreateBootstrapLogger();
-
 try
 {
     IHost host = Host.CreateDefaultBuilder(args)
+        // appsettings.json の Serilog キーからログ出力の設定を読み込む
         .UseSerilog((context, _, loggerConfig) =>
             loggerConfig.ReadFrom.Configuration(context.Configuration))
+        // メッセージファイルをコンフィグとして読み込みできるようにする
         .ConfigureAppConfiguration((_, config) =>
         {
-            // メッセージファイルをコンフィグ形式で読み込みできるようにする
             config.AddJsonFile(
                 Path.Combine(AppContext.BaseDirectory, "data", "message.json"),
                 optional: false,
@@ -46,13 +47,11 @@ try
                                      GatewayIntents.GuildMessages
                 }));
 
+            // スラッシュコマンドを属性ベース（SlashCommand 属性）で実装できるようにする
             services.AddSingleton(provider =>
                 new InteractionService(
                     provider.GetRequiredService<DiscordSocketClient>(),
-                    new InteractionServiceConfig()
-                    {
-                        //
-                    }));
+                    new InteractionServiceConfig()));
 
             services.AddHostedService<NtpTimeCheckService>();
             services.AddSingleton<MoveService>();
