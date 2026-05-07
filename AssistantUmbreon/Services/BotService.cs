0e32eeff9e855633b0099ae2ae7aa7769700e6ad
Tana <finaltruth.shintai@gmail.com>
2026-05-07 18:32:21 +0900

    名前空間を修正。

--- AssistantUmbreon/Services/BotService.cs
+++ AssistantUmbreon/Services/BotService.cs
@@ -5,8 +5,11 @@ using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
-namespace AssistantUmbreon;
+namespace AssistantUmbreon.Services;
 
+/// <summary>
+/// Discord BOT の開始から終了までのライフサイクルを実装します。
+/// </summary>
 public class BotService : IHostedService
 {
     private readonly DiscordSocketClient _client;
@@ -14,6 +17,13 @@ public class BotService : IHostedService
     private readonly IServiceProvider _services;
     private readonly IConfiguration _config;
 
+    /// <summary>
+    /// <see cref="BotService"/> の新しいインスタンスを生成します。
+    /// </summary>
+    /// <param name="client">Web Socket ベースの Discord クライアント。</param>
+    /// <param name="interactions"></param>
+    /// <param name="services"></param>
+    /// <param name="config"></param>
     public BotService(DiscordSocketClient client, InteractionService interactions, IServiceProvider services, IConfiguration config)
     {
         _client = client;
@@ -43,11 +53,18 @@ public class BotService : IHostedService
         await _client.StopAsync();
     }
 
+    /// <summary>
+    /// Discord クライアントが接続完了になった時に呼び出されます。
+    /// </summary>
+    /// <returns></returns>
     private async Task OnReadyAsync()
     {
+        // スラッシュコマンドの登録対象サーバーを取得する
         var guildIdStr = Environment.GetEnvironmentVariable("GUILD_ID")
             ?? throw new InvalidOperationException("環境変数 'GUILD_ID' が設定されていません。");
         var guildId = ulong.Parse(guildIdStr);
+
+        // サーバーにスラッシュコマンドを登録する
         await _interactions.RegisterCommandsToGuildAsync(guildId);
     }
 
