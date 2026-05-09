c1c3cc500beec6a6ea16ea873306519e5f418999
Tana <finaltruth.shintai@gmail.com>
2026-05-09 10:06:57 +0900

    解説コメントを整理。

--- AssistantUmbreon/Program.cs
+++ AssistantUmbreon/Program.cs
@@ -1,4 +1,3 @@
-using AssistantUmbreon;
 using AssistantUmbreon.Services;
 using Discord;
 using Discord.Interactions;
@@ -9,23 +8,26 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
-// ホスト構築前のエラーをコンソールに出力するために、最小構成のロガーを作成して使用する
+// ホスト構築前のエラーをコンソールに出力するために、最小構成のログ出力を構築する
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateBootstrapLogger();
 
 #if DEBUG
 // 開発ビルド時のみ .env ファイルがあれば読み込んで環境変数として使用できるようにする
-if (File.Exists(".env"))
-{
-    Env.Load();
-}
+if (File.Exists(".env")) { Env.Load(); }
 #endif
 
 try
 {
+    // ホスト型のアプリケーションとして構築する
+    // Tips:
+    // - IHost.RunAsync メソッドを呼び出すことで、 IHost.Services プロパティ（DI コンテナ）に登録した IHostedService オブジェクトを自動的に開始する
+    // - アプリケーションが終了するまで IHostedService オブジェクトはホストされ、自動的に停止の試みが行われる仕組み
+    // - Host.CreateDefaultBuilder メソッドで構成される既定の設定は以下のページを参照
+    //   https://learn.microsoft.com/ja-jp/dotnet/api/microsoft.extensions.hosting.host.createdefaultbuilder?view=net-10.0-pp#remarks
     IHost host = Host.CreateDefaultBuilder(args)
-        // appsettings.json の Serilog キーからログ出力の設定を読み込む
+        // appsettings.json の Serilog キーに記述した設定を読み込み、ログ出力を構築する
         .UseSerilog((context, _, loggerConfig) =>
             loggerConfig.ReadFrom.Configuration(context.Configuration))
         // メッセージファイルをコンフィグとして読み込みできるようにする
@@ -39,6 +41,7 @@ try
         // DI コンテナに必要なサービスを登録する
         .ConfigureServices((_, services) =>
         {
+            // WebSocket ベースの Discord クライアントを登録する
             services.AddSingleton(new DiscordSocketClient(
                 new DiscordSocketConfig()
                 {
@@ -47,14 +50,17 @@ try
                                      GatewayIntents.GuildMessages
                 }));
 
-            // スラッシュコマンドを属性ベース（SlashCommand 属性）で実装できるようにする
+            // 属性ベース（SlashCommand 属性）で実装したスラッシュコマンドのサービスを登録する
             services.AddSingleton(provider =>
                 new InteractionService(
                     provider.GetRequiredService<DiscordSocketClient>(),
                     new InteractionServiceConfig()));
 
-            services.AddHostedService<NtpTimeCheckService>();
+            // Hack: DIコンテナ化する必要ある？
             services.AddSingleton<MoveService>();
+
+            // IHostedService オブジェクトを登録する
+            services.AddHostedService<NtpTimeCheckService>();
             services.AddHostedService<BotService>();
         })
         .Build();
